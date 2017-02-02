a1f1059a1efa0c6fa0f7470d258eb829b340d43b
Philip Knape <philip.knape@gmail.com>
2017-02-02 12:07:40 +0100

    utility method for moving string in array

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const shuffle = (array) => {
-  const d = array;
+  const d = array.slice();
   for (let c = d.length - 1; c > 0; c--) {
     const b = Math.floor(Math.random() * (c + 1));
     const a = d[c];
@@ -7,4 +7,9 @@ export const shuffle = (array) => {
     d[b] = a;
   }
   return d;
+};
+
+
+export const move = (array, steps = 1) => {
+  return [].concat(array.slice(steps), array.slice(0, steps));
 };
