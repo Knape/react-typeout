8e69b72905f4139744d203d97870bf4ec3aa19c9
Philip Knape <philip.knape@gmail.com>
2017-06-18 21:02:41 +0200

    adds prop-types to its own dep

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { shuffle, move, getFirst } from './utils';
+
 class TypeOut extends Component {
 
   constructor(props) {
